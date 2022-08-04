40d9e7d1144c0c15c752bb38e971083b0cad6d06
kenta66448284 <92067021+kenta66448284@users.noreply.github.com>
2022-08-04 16:07:46 +0900

    作品検索機能の追加
    画像の表示の設定も行いました

--- js/ziburi.js
+++ js/ziburi.js
@@ -1,19 +1,42 @@
+
+
 const url = `https://ghibliapi.herokuapp.com/films`;
         const characterurl=[];
+        const movielist =[];
         let ul = document.querySelector("ul");
-        let h1 = document.querySelector("h2")
+        let h2 = document.querySelector("h2")
+        let image= document.querySelector("img")
         fetch(url)
         .then(function(response){
             return response.json();
         })
         .then(function(ziburi){
             console.log(ziburi)
-            let character = ziburi[2].people;
-            h1.innerHTML=ziburi[2].original_title
+            let movie = document.querySelector(".movie")
+            for(let i=0;i<ziburi.length;i++){
+                movielist[i]=ziburi[i].original_title;
+                let list = document.createElement("option")
+                list.setAttribute("value",i)
+                list.innerHTML= movielist[i];
+                movie.appendChild(list)
+            }
+            console.log(movielist)
+            let ml = document.querySelectorAll("option")
+            let button = document.querySelector(".button")
+            button.addEventListener("click",function(){
+                ul.innerHTML=""
+                let mnumber = movie.value
+                console.log(mnumber);
+                let character = ziburi[mnumber].people;
+                let imgcode = ziburi[mnumber].image
+                image.setAttribute("src",imgcode)
+
+            h2.innerHTML=ziburi[mnumber].original_title
             console.log(character)
             for(let i=0;i<=character.length;i++){
                 
                 characterurl[i] = character[i];
+                console.log(characterurl)
                 fetch(characterurl[i])
                 .then(function(response){
                     return response.json();
@@ -26,4 +49,7 @@ const url = `https://ghibliapi.herokuapp.com/films`;
                     ul.appendChild(li);
                 })
             }
+            })
+
+            
         })
